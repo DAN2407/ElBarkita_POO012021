fb988f84f865b42672eccef792ddf394bbcc5ac6
DAN2407 <00042220@uca.edu.sv>
2021-03-16 14:21:29 -0600

    Elaborate changes

---
+++ Barkita012021/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Barkita012021
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+
+            Console.WriteLine("Enter a value for a :");
+            a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a value for b :");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Adding {a} + {b} equals {Addnumbers(a,b)} ");
+        }
+
+        public static int Addnumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
